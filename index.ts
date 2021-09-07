96b5c515f289ca4d1a34f7a2aa29ed494e474c2b
すずねーう <67305123+waki285@users.noreply.github.com>
2021-09-07 11:29:04 +0000

    Initial commit

--- index.ts
+++ index.ts
@@ -4,7 +4,11 @@ import {
   TextChannel,
   TextBasedChannel,
   TextBasedChannels,
-  Channel
+  Channel,
+  Snowflake,
+  MessagePayload,
+  WebhookMessageOptions,
+  MessageOptions
 } from "discord.js";
 
 let client:Client | undefined;
@@ -15,16 +19,32 @@ function setup(cli:Client) {
 
 class SGCClient {
   private readonly channel_v1:TextBasedChannels; 
+  private readonly channel_v2:TextBasedChannels; 
+  private readonly isWebhook:boolean;
+  private readonly messageData:MessageOptions | MessagePayload | WebhookMessageOptions
+  /**
+   * 
+   * @param {Snowflake} channel_v1 Super Global Chat v1でしようするチャンネル。
+   * @param {Snowflake} channel_v2S uper Global Chat v2でしようするチャンネル。
+   * @param {boolean} isWebhook 送信にウェブフックを使用するかどうか。
+   */
   constructor(
-    channel_v1:string,
-    channel_v2:string
+    channel_v1:Snowflake,
+    channel_v2:Snowflake,
+    isWebhook:boolean,
+    messageData:MessageOptions | MessagePayload | WebhookMessageOptions
   ) {
     if (!client) throw new ReferenceError("client is not defined");
     const temp:Channel | undefined = client.channels.cache.get(channel_v1);
     if (!temp) throw new ReferenceError("channel_v1 channel not found");
     if (!temp.isText()) throw new ReferenceError("channel_v1 channel is not a text channel")
     this.channel_v1 = temp;
-    
+    const temp2:Channel | undefined = client.channels.cache.get(channel_v2);
+    if (!temp) throw new ReferenceError("channel_v1 channel not found");
+    if (!temp.isText()) throw new ReferenceError("channel_v1 channel is not a text channel")
+    this.channel_v2 = temp;
+    this.isWebhook = isWebhook;
+    this.messageData = messageData;
   }
 }
 
